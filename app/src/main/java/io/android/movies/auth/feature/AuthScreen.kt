37c2d5f766a142cf209a7575945e04c13d8074d9
ShiryaevNikolay <shiryaev@surfstudio.ru>
2024-03-03 16:06:41 +0300

    Отображение экрана авторизации

--- app/src/main/java/io/android/movies/auth/feature/AuthScreen.kt
+++ app/src/main/java/io/android/movies/auth/feature/AuthScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -17,6 +18,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -34,9 +37,9 @@ fun AuthScreen() {
     ) {
         Text(
             text = stringResource(id = R.string.auth_title),
-            fontSize = 18.sp
+            fontSize = 20.sp
         )
-        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(20.dp))
         TextField(
             value = email,
             label = {
@@ -57,6 +60,8 @@ fun AuthScreen() {
                 )
             },
             onValueChange = { password = it },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            visualTransformation = PasswordVisualTransformation(),
             modifier = Modifier
                 .fillMaxWidth()
         )
